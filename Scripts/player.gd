0db42559e978044be04295b4267a37bbc9032930
RancidTea <rancidtea101@gmail.com>
2024-03-13 08:14:20 +0000

    Fixed the snapping bug
    - fixed the snapping bug of the anim tree

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -9,6 +9,7 @@ var current_state = States.IDLE
 @onready var anim_state = anim_tree.get("parameters/playback")
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var last_direction = 0
 
 enum States { IDLE, MOVE, JUMP, SWALLOW, DEATH }
 
@@ -17,14 +18,16 @@ func _ready():
 	
 func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
-
-	anim_tree.set("parameters/Hit/blend_position", direction)
-	anim_tree.set("parameters/Die/blend_position", direction)
+	if direction != 0:
+		last_direction = direction
+	var anim_direction = direction if direction != 0 else last_direction
+	anim_tree.set("parameters/Hit/blend_position", anim_direction)
+	anim_tree.set("parameters/Die/blend_position", anim_direction)
 
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		if velocity.y > 0:
-			anim_tree.set("parameters/Fall/blend_position", direction)
+			anim_tree.set("parameters/Fall/blend_position", anim_direction)
 			anim_state.travel("Fall")
 			if is_on_floor():
 				change_state(States.IDLE)
@@ -33,7 +36,7 @@ func _physics_process(delta):
 		change_state(States.JUMP)
 		in_air = true
 	elif current_state == States.JUMP and is_on_floor():
-		anim_tree.set("parameters/Idle/blend_position", direction)
+		anim_tree.set("parameters/Idle/blend_position", anim_direction)
 		change_state(States.IDLE)
 	if Input.is_action_just_pressed("ui_right") or Input.is_action_just_pressed("ui_left"):
 		change_state(States.MOVE)
@@ -63,7 +66,10 @@ func change_state(new_state):
 	current_state = new_state
 
 func handle_jump(direction):
-	anim_tree.set("parameters/Jump/blend_position", direction)
+	if direction != 0:
+		var last_direction = direction
+	var anim_direction = direction if direction != 0 else last_direction
+	anim_tree.set("parameters/Jump/blend_position", anim_direction)
 	velocity.y = JUMP_VELOCITY
 	if(Input.is_action_just_pressed("ui_up")):
 		anim_state.travel("Jump")
@@ -71,7 +77,10 @@ func handle_jump(direction):
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 
 func handle_move(direction):
-	anim_tree.set("parameters/Move/blend_position", direction)
+	if direction != 0:
+		var last_direction = direction
+	var anim_direction = direction if direction != 0 else last_direction
+	anim_tree.set("parameters/Move/blend_position", anim_direction)
 	velocity.x = direction * SPEED
 	if direction != 0:
 		if is_on_floor():
@@ -81,12 +90,15 @@ func handle_move(direction):
 	else:
 		if is_on_floor() and current_state != States.IDLE:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
-			anim_tree.set("parameters/Idle/blend_position", direction)
+			anim_tree.set("parameters/Idle/blend_position", anim_direction)
 			anim_state.travel("Idle")
 			change_state(States.IDLE)
 
 func handle_swallow(direction):
-	anim_tree.set("parameters/Swallow/blend_position", direction)
+	if direction != 0:
+		var last_direction = direction
+	var anim_direction = direction if direction != 0 else last_direction
+	anim_tree.set("parameters/Swallow/blend_position", anim_direction)
 	if(Input.is_action_pressed("ui_down")):
 		anim_state.travel("Swallow")
 	if is_on_floor() and direction == 0:
